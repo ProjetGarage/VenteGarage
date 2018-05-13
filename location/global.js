aef177afc0a3f9a5485c3f95e8e0d6669dd966fc
alejjimn <alejjimn@gmail.com>
2018-05-13 16:02:06 -0400

    location update

--- location/global.js
+++ location/global.js
@@ -1,4 +1,12 @@
 function main()
 {
+    initMap();
     charger();
 }
+  function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 10,
+          center: new google.maps.LatLng(45.5,-73),
+          mapTypeId: 'terrain'
+        });
+      }
